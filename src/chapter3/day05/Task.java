621b06ccdbf75bb0fff6291f781051c32d80e7b0
ShiminLei <sl2kd@virginia.edu>
2020-04-01 19:43:53 -0400

    new

--- src/chapter3/day05/Task.java
+++ src/chapter3/day05/Task.java
@@ -1,11 +1,53 @@
 package chapter3.day05;
 
+import java.util.Scanner;
+
 public class Task {
     public static void main(String[] args) {
         palindrome("12322");
+        swap(10, 89);
+        cost();
     }
 
 
+
+    /**
+     * 计算打折金额
+     */
+    private static void cost(){
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        double cost;
+        if (m<0){
+            System.out.println("请输入合法数值");
+        }else{
+            if (m<=2000){
+                cost = m*0.9;
+            }else if (m<=4000){
+                cost = m*0.8;
+            }else if (m<=8000){
+                cost = m*0.75;
+            }else{
+                cost = m*0.7;
+            }
+            System.out.println("打折后的金额是"+cost);
+        }
+    }
+
+    /**
+     * 互换两个整数
+     * @param a
+     * @param b
+     */
+    private static void swap(int a, int b){
+        System.out.println(a);
+        System.out.println(b);
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.println(a);
+        System.out.println(b);
+    }
 
     /**
      * 判断回文数
