37460f1e86c21b80664b5bab31dcb5a5d4e770bb
ShiminLei <sl2kd@virginia.edu>
2020-04-02 08:40:17 -0400

    new

--- src/chapter3/day05/Task.java
+++ src/chapter3/day05/Task.java
@@ -1,15 +1,238 @@
 package chapter3.day05;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Task {
     public static void main(String[] args) {
-        palindrome("12322");
-        swap(10, 89);
-        cost();
+//        palindrome("12322");
+//        swap(10, 89);
+//        cost();
+//        sum(254);
+//        plot();
+//        prime();
+//        inverse(452);
+//        fibo(3);
+//        twoSum(new int[]{1,2,3}, 3);
+//        shuangseqiu();
+//        duizhang(6);
+
     }
 
 
+
+    /**
+     * 选队长
+     * @param n
+     */
+    private static void duizhang(int n){
+        int left = n;
+        int i = -1;
+        boolean[] f = new boolean[n];
+        while (left > 1){
+            for (int j=0; j<3; j++){
+                i = step(f, i);
+                while (f[i]==true){
+                    i = step(f, i);
+                }
+            }
+            f[i] = true;
+//            System.out.println(i+1);
+            left--;
+        }
+        while (f[i]==true){
+            i = step(f, i);
+        }
+        System.out.println(i+1);
+    }
+
+    /**
+     * 选队长-辅助函数
+     * @param f
+     * @param i
+     * @return
+     */
+    private static int step(boolean[] f, int i){
+        if (i<f.length-1){
+            i++;
+        }else{
+            i = 0;
+        }
+        return i;
+    }
+
+
+
+    /**
+     * 双色球游戏
+     */
+    private static void shuangseqiu(){
+        int[] my = new int[7];
+        int[] nums = new int[7];
+        Set<Integer> seen = new HashSet<>();
+        System.out.println("请输入你买的7个彩票数字");
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        for (int i=0; i<7; i++){
+            my[i] = sc.nextInt();
+            if (i!=7){
+                 int temp = rd.nextInt(33)+1;
+                 while (seen.contains(temp)){
+                     temp = rd.nextInt(33)+1;
+                 }
+                 nums[i] = temp;
+                 seen.add(temp);
+            }else {
+                nums[i] = rd.nextInt(16)+1;
+            }
+        }
+        System.out.println("你买的号码是");
+        for (int num: my){
+            System.out.print(""+num + " ");
+        }
+        System.out.println("\n中奖号码是");
+        for (int num: nums){
+            System.out.print(""+num + " ");
+        }
+        //
+        int count1 = 0;
+        for (int i=0; i<6; i++){
+            if (seen.contains(my[i])){
+                count1++;
+            }
+        }
+        int count2 = 0;
+        if (my[6]==nums[6]){
+            count2++;
+        }
+        System.out.println("您的中奖情况是：");
+        if (count1==6 && count2==1){
+            System.out.println("一等奖");
+        }else if (count1==6 && count2==0){
+            System.out.println("二等奖");
+        }else if (count1==5 && count2==1){
+            System.out.println("三等奖");
+        }else if (count1==5 || (count1==4 && count2==1)){
+            System.out.println("四等奖");
+        }else if (count1==4 || (count1==3 && count2==1)){
+            System.out.println("五等奖");
+        }else if (count2==1 && (count1>=0 && count1<=2)){
+            System.out.println("六等奖");
+        }else{
+            System.out.println("没中奖");
+        }
+    }
+
+    /**
+     * 两数之和
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<n; i++){
+            if (map.get(target-nums[i])!=null){
+                return new int[]{map.get(target-nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1,-1};
+    }
+
+    /**
+     * 斐波那契数列
+     * @param n
+     */
+    private static void fibo(int n){
+        int[] f = new int[n];
+        f[0] = 0;
+        System.out.println(0);
+        if (n==1)return;
+        f[1] = 1;
+        System.out.println(1);
+        if (n==2)return;
+        for (int i=2; i<n; i++){
+            f[i] = f[i-1] + f[i-2];
+            System.out.println(f[i]);
+        }
+
+    }
+
+
+    /**
+     * 逆序打印数字
+     * @param num
+     */
+    private static void inverse(int num){
+        int count = 1;
+        while (num>=10){
+            int mod = num%10;
+            num = num/10;
+            System.out.println(mod);
+            count++;
+        }
+        System.out.println(num);
+        System.out.println("总共的位数是"+count);
+    }
+
+    /**
+     * 找100 以内的质数
+     */
+    private static void prime(){
+        System.out.println(2);
+        for (int i=3; i<=100;i++){
+            boolean flag = true;
+            for (int j=2; j*j<=i; j++ ){
+                if (i%j==0 && i!=j){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) System.out.println(i);
+        }
+    }
+
+    /**
+     * 画菱形
+     */
+    private static void plot(){
+        for (int i=0; i<=4; i++){
+            for (int j=4-i; j>=0; j--){
+                System.out.print(' ');
+            }
+            for (int j=0; j<i*2-1; j++){
+                System.out.print('*');
+            }
+            System.out.print('\n');
+        }
+        for (int i=2; i>=0; i--){
+            for (int j=0; j<4-i; j++){
+                System.out.print(' ');
+            }
+            for (int j=i*2; j>=0; j--){
+                System.out.print('*');
+            }
+            System.out.print('\n');
+        }
+    }
+
+    /**
+     * 计算各位之和
+     * @param num
+     * @return
+     */
+    private static int sum(int num){
+        int sum = 0;
+        while (num>=10){
+            int mod = num%10;
+            num = num/10;
+            sum += mod;
+        }
+        sum += num;
+        System.out.println(sum);
+        return sum;
+    }
 
     /**
      * 计算打折金额
---
+++ src/chapter3/day05/Wuziqi.java
@@ -0,0 +1,108 @@
+package chapter3.day05;
+import java.util.Scanner;
+
+public class Wuziqi {
+    public static void main(String[] args) {
+        //1.声明一个GoBand类型的引用指向该类的对象
+        Wuziqi gb = new Wuziqi();
+        //2.调用成员方法来绘制棋盘
+        gb.paint();
+        //3.调用成员方法来进行下棋
+        gb.play();
+
+    }
+    //自定义二维数组来描述棋盘，默认初始值为0
+    int[][] chessBoard = new int[16][16];
+
+    //自定义成员方法来绘制棋盘
+    void paint(){
+        //1.先绘制棋盘中第一行的坐标信息，也就是列坐标信息
+        for(int i = 0; i < 17; i++){
+            if(i == 0){
+                System.out.print("  ");
+            }
+            else{
+                //按照十六进制的格式打印i-1的数值
+                System.out.printf("%x ", i-1);
+            }
+        }
+        System.out.println();
+
+        //2.绘制棋盘中除了第一行之外的其他部分以及行坐标信息
+        for(int i = 0; i < 16; i++){
+            //用于打印行坐标信息
+            System.out.printf("%x ", i);
+            for(int j = 0; j < 16; j++){
+                //刚开始棋盘中的所有内容都是+，因此直接打印
+                if(chessBoard[i][j] == 0){
+                    System.out.print("+ ");
+                }
+                else if(chessBoard[i][j] == 1){
+                    System.out.print("● ");
+                }
+                else{
+                    System.out.print("○ ");
+                }
+            }
+            //打印完毕一行的所有内容之后进行换行
+            System.out.println();
+        }
+    }
+
+    //自定义成员方法来提示黑方和白方分别下棋
+    void play(){
+        //定义标志位来进行黑方和白方的切换，true代表黑方，false代表白方
+        boolean flag = true;
+        //不断地分别提示黑方和白方下棋
+        while(true){
+            System.out.println("请" + (flag ? "黑方": "白方") + "输入落子坐标(x y)：");
+            Scanner sc = new Scanner(System.in);
+            int ix = sc.nextInt();
+            int iy = sc.nextInt();
+            //根据用户输入的坐标来调整棋盘中的图案，策略为改变数组的元素值
+            if(flag){
+                //当黑方落子时就将数组中对应元素值改为1
+                chessBoard[ix][iy] = 1;
+            }else{
+                //当白方落子时就将数组中对应元素改为2
+                chessBoard[ix][iy] = 2;
+            }
+            //重新绘制图案
+            paint();
+            //判断当前方是否胜利，若胜利就立刻结束游戏
+            if(judge(ix, iy)){
+                System.out.println("恭喜" + (flag ? "黑方": "白方") + "胜利了！");
+                break;
+            }
+            //此时切换下棋方
+            flag = !flag;
+        }
+    }
+
+    //自定义成员方法来判断用户是否获胜，获胜的规则是：任意相同颜色的5个棋子连成一线
+    boolean judge(int ix, int iy){
+        //1.判断竖向是否连成一线，则需要以该点为中心向上四个点向下四个点
+        //声明变量来统计竖向相同颜色棋子的个数，先统计向上同色棋子的个数
+        int count = 1;
+        for(int i = ix-1; i >= 0 && i >= ix-4; i--){
+            //若当前点代表的棋子与上述某个点代表的棋子不一样，则向上统计结束
+            if(chessBoard[ix][iy] != chessBoard[i][iy]){
+                break;
+            }
+            count++;
+        }
+        System.out.println("count1 = " + count);
+        //再统计向下颜色相同的个数
+        for(int i = ix+1; i <= 15 && i <= ix+4; i++){
+            if(chessBoard[ix][iy] != chessBoard[i][iy]){
+                break;
+            }
+            count++;
+        }
+        System.out.println("count2 = " + count);
+        //... ...
+        return count >= 5;
+
+        //当所有可能胜利的情况都排除了，那么肯定是失败了
+    }
+}
