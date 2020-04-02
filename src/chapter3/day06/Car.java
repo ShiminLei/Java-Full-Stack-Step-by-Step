268c1fd8e20c0bda16d8e0c5fb7b719be8d25fe9
ShiminLei <sl2kd@virginia.edu>
2020-04-02 19:55:08 -0400

    new

--- src/chapter3/day06/Car.java
+++ src/chapter3/day06/Car.java
@@ -9,6 +9,30 @@ public class Car {
         this.color = color;
     }
 
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
     public void show(){
         System.out.println("品牌是"+this.brand);
         System.out.println("颜色是"+this.color);
